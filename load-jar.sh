b7b93d19e0f76d58dfc36e9d1867b0cc25b5b9f5
boon4681 <46645814+boon4681@users.noreply.github.com>
2022-09-17 22:54:04 +0700

    forgot that i'm not using docker

---
+++ load-jar.sh
@@ -0,0 +1,4 @@
+mkdir /itemsflower
+cd /itemsflower
+wget https://github.com/intoolswetrust/jd-cli/releases/download/jd-cli-1.2.0/jd-cli-1.2.0-dist.tar.gz
+tar -xvf jd-cli-1.2.0-dist.tar.gz jd-cli.jar
