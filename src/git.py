46c86569f7afcf21cf861469842135c70eb763b8
boon4681 <46645814+boon4681@users.noreply.github.com>
2022-09-25 16:05:47 +0700

    yo

--- src/git.py
+++ src/git.py
@@ -11,20 +11,20 @@ class Git:
     def init(self):
         os.chdir(self.source.make(self.version).parsed.as_posix())
         subprocess.run(['git','init','-q'])
-        subprocess.run(['git', 'checkout', '-q', '-b', self.version], capture_output=True)
+        subprocess.run(['git', 'checkout', '-q', '-b', 'data'], capture_output=True)
         subprocess.run(['git', 'config', 'user.name', 'actions-user'])
         subprocess.run(['git', 'config', 'user.email', 'actions@github.com'])
         tags = subprocess.run(['git','remote'], capture_output=True).stdout.decode('utf-8').split('\n')
         add_or_not = 'origin' in tags
         subprocess.run(['git', 'remote', 'set-url' if add_or_not else 'add' ,'origin',f'https://x-access-token:{os.getenv("github-token")}@github.com/{os.getenv("github-repository")}'])
-        subprocess.run(['git', 'fetch', '-q', '--tags', 'origin', self.version])
+        subprocess.run(['git', 'fetch', '-q', '--tags', 'origin', 'data'])
         self.is_init = True
 
     def commit(self):
         if(not self.is_init):
             return 'ERROR'
         subprocess.run(['git', 'add', '.'])
-        subprocess.run(['git', 'commit', '-q', '-m', '🚀 boom burst the big bang'])
+        subprocess.run(['git', 'commit', '-q', '-m', '🍀 wow clover'])
         subprocess.run(['git', 'tag', '-f', self.version])
-        subprocess.run(['git', 'push','-f','-q', '--tags', 'origin'])
+        subprocess.run(['git', 'push','-f','-q', '--tags', 'origin','data'])
         os.chdir('..')
